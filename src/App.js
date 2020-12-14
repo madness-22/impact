1f30841f3ff9436950e2049938ba2b3c7f3a790d
madness_22 <mohammadzughni@gmail.com>
2020-12-14 09:51:54 +0200

    todo list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Checkbox from './com/check/Checkbox'
+
+const tasks =[
+  'hi',
+  'my',
+  'name',
+  'name',
+  'is',
+  'VaLaK'
+]
+
 
 function App(){
   return(
   <div>
-      <h1>
-        this is develop branch
-      </h1>
+    <h1>
+      To DO
+    </h1>
+    <ul>
+
+     {tasks.map((task) => <Checkbox text={task} />)}
+    </ul>
+  
+      
+      
   </div>
   )};
 
 
 
 export default App;
+
+
+
+// array.forEach(element => {
+  
+// });
+
+// map 
