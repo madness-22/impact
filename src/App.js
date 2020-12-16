0201cb8d632dcb36a60861d9f697ca18988dba5d
madness_22 <mohammadzughni@gmail.com>
2020-12-16 11:39:46 +0200

    grod card

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-// import Checkbox from './com/check/Checkbox'
+import 'antd/dist/antd.css';
+// import React, {useState} from "react"
+import Mycard from './com/Card/Card'
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { Card } from 'antd';
+
+
+const { Header, Content, Footer } = Layout;
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">Profile</Menu.Item>
+        <Menu.Item key="2">Mail</Menu.Item>
+        <Menu.Item key="3">Setting</Menu.Item>
+      </Menu>
+    </Header>
+    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+      <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+        < Mycard />
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+  </Layout>
   );
 }
 
 export default App;
+
+
+
 
 
---
+++ src/com/Card/Card.js
@@ -0,0 +1,39 @@
+
+import "./Mycard.css";
+import { Card } from 'antd';
+import { Row, Col } from 'antd';
+
+const Mycard=() =>{
+    return(
+    <div className="site-caed-wrapper">
+        <Row justify="center" gutter={16}>
+            <Col span={8}>
+            <Card title="Bio card" >
+                <p>Name: VaLaK</p>
+                <p>Contry: Libya</p>
+                <p>City: Tripoli</p>
+                </Card> 
+            </Col>
+            <Col span={8} >
+            <Card title="Bio card"  >
+                <p>Name: VaLaK</p>
+                <p>Contry: Libya</p>
+                <p>City: Tripoli</p>
+                </Card> 
+            </Col>
+            <Col span={8}>
+            <Card title="Bio card"  >
+                <p>Name: VaLaK</p>
+                <p>Contry: Libya</p>
+                <p>City: Tripoli</p>
+                </Card> 
+            </Col>
+        </Row>,
+    </div>
+    )};
+
+
+export default Mycard
+
+
+ 
